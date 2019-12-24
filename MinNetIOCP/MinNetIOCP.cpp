40822efebb3580fb856609a8e362263f184edb05
dat0613 <dat0613@naver.com>
2019-12-24 10:30:11 +0900

    MySQL 테스트 끝
    다음은 p2p다

--- MinNetIOCP/MinNetIOCP.cpp
+++ MinNetIOCP/MinNetIOCP.cpp
@@ -166,13 +166,21 @@ void MinNetIOCP::ServerLoop()
 {
 	float sleep_time = 1000.0f / (float)tick;
 	clock_t last_heart_beat = 0;
+	clock_t lastUpdate = 0;
 	clock_t cur_time = 0;
 
 	while (true)
 	{
+		cur_time = clock();
+		
 		MinNetMySQL::IOprocessing();// 데이터베이스와의 작업은 싱글 스레드에서만 함 멀티 스레드 로 하기에는 아직 경험이 없음
 
-		cur_time = clock();
+		if (cur_time - lastUpdate <= sleep_time)
+		{
+			continue;
+		}
+
+		lastUpdate = cur_time;
 
 		if (cur_time - last_heart_beat > 3000)
 		{
@@ -203,9 +211,6 @@ void MinNetIOCP::ServerLoop()
 
 		room_manager.Update();
 		MinNetTime::FrameEnd();
-
-
-		_sleep(sleep_time);
 	}
 }
 
